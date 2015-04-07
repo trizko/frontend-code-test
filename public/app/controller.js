00bc38cc06434bf7877fd49f8afc741c117c0b5c
Tony Rizko <tony@rizkocircle.com>
2015-04-07 10:52:29 -0700

    get list of distinct ingredients for selected recipes

--- public/app/controller.js
+++ public/app/controller.js
@@ -11,6 +11,7 @@
       vm.recipes = null;
       vm.ingredients = [];
       vm.selectedRecipes = [];
+      vm.ingredientsNeeded = [];
       vm.toggleSelection = toggleSelection;
 
       $http.get('../content/recipes.json').success(function(data){
@@ -40,7 +41,25 @@
           vm.selectedRecipes.push(recipeName);
         }
 
-        console.log(vm.selectedRecipes);
+        vm.ingredientsNeeded = getIngredientsForSelectedRecipes(vm.selectedRecipes);
+      }
+
+      function getIngredientsForSelectedRecipes(recipes){
+        var ingredientsNeeded = [];
+
+        for(var i = 0; i < recipes.length; i++){
+          for(var j = 0; j < vm.recipes.length; j++){
+            if(recipes[i] === vm.recipes[j].name){
+              for(var k = 0; k < vm.recipes[j].ingredients.length; k++){
+                if(ingredientsNeeded.indexOf(vm.recipes[j].ingredients[k]) === -1){
+                  ingredientsNeeded.push(vm.recipes[j].ingredients[k]);
+                }
+              }
+            }
+          }
+        }
+
+        return ingredientsNeeded;
       }
     }
 
