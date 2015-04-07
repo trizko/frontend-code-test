adbd49fe4fa3918aed0454772953c7a05c6f6abd
Tony Rizko <tony@rizkocircle.com>
2015-04-07 09:35:00 -0700

    get unique list of ingredients from json data

--- public/app/controller.js
+++ public/app/controller.js
@@ -13,7 +13,22 @@
 
       $http.get('../content/recipes.json').success(function(data){
         vm.recipes = data;
+        vm.ingredients = getUniqueIngredientsList(vm.recipes);
       });
+
+      function getUniqueIngredientsList(recipes){
+        var results = [];
+
+        for(var i = 0; i < recipes.length; i++){
+          for(var j = 0; j < recipes[i].ingredients.length; j++){
+            if(results.indexOf(recipes[i].ingredients[j]) === -1){
+              results.push(recipes[i].ingredients[j]);
+            }
+          }
+        }
+
+        return results;
+      }
     }
 
 })();
